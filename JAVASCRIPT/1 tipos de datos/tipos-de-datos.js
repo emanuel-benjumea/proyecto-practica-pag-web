82360d3340bcc36b6325833e8fcda67ffe7e0288
emanuel-benjumea <66340501+emanuel-benjumea@users.noreply.github.com>
2021-06-04 22:45:21 -0500

    tipos de datos
    estudiamos los tipos de datos que existen en js

--- JAVASCRIPT/1 tipos de datos/tipos-de-datos.js
+++ JAVASCRIPT/1 tipos de datos/tipos-de-datos.js
@@ -1,1 +1,69 @@
+
+//tipo de dato string
 var nombre ="emanuel";
+console.log(nombre);
+console.log(typeof nombre);
+
+
+//redefinir una variable
+nombre = 10
+console.log(nombre);
+console.log(typeof nombre);
+
+
+//tipo de dato numerico
+
+var numero = 1000;
+console.log(numero);
+
+//tipo de dato object
+
+var objeto ={
+    nombre : "juan",
+    apellido : "lopez",
+    telefono : "1234"
+}
+
+console.log(objeto);
+console.log(typeof objeto);
+
+
+//tipo de dato bolleano
+
+var bandera=true;
+console.log(bandera);
+
+var falso= false;
+console.log(falso);
+
+//tipo de dato funcion
+
+function mifuncion(){}
+
+console.log(mifuncion);
+
+
+//tipo de dato
+
+var simbolo =Symbol("mi simbolo");
+console.log(simbolo);
+
+
+//las clases son funciones
+class persona{
+    constructor(nombre,apellido){
+    this.nombre =nombre;
+    this.apellido=apellido
+    }
+}
+console.log(typeof persona);
+
+//undifined
+var x;
+console.log(x);
+
+
+//null
+var y=null;
+console.log(y);
+console.log(typeof y);
